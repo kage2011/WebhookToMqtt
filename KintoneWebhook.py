e2b6f6626180fe46b116aaa700465d1c26da17a8
kage2011 <kage2011_notice@yahoo.co.jp>
2024-10-31 10:34:20 +0900

    render用に変更

--- KintoneWebhook.py
+++ KintoneWebhook.py
@@ -1,19 +1,19 @@
+import os
 import paho.mqtt.client as mqtt
 import json
 from flask import Flask, request
 
 app = Flask(__name__)
-# MQTT設定
-mqtt_broker = "broker.hivemq.com"
-mqtt_port = 8884
-mqtt_topic = "kintone/comment/webhook"
+
+# 環境変数からMQTT設定を取得
+mqtt_broker = os.getenv("MQTT_BROKER", "broker.hivemq.com")
+mqtt_port = int(os.getenv("MQTT_PORT", 8884))
+mqtt_topic = os.getenv("MQTT_TOPIC", "kintone/comment/webhook")
 
 # MQTTクライアントの設定
 client = mqtt.Client(transport="websockets")
 client.tls_set()
 client.connect(mqtt_broker, mqtt_port, 60)
-
-print("test")
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -24,4 +24,3 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(port=5000)
-
